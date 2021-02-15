72adf2184fd147d530a6d8e664c64b083e3f9a05
Siddharth Subramanian <sidsbrmnn@gmail.com>
2021-02-15 16:55:02 +0530

    Migrate to Tailwind CSS 2.0

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  plugins: [
-    require("tailwindcss")("./tailwind.config.js"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production"
-      ? [require("cssnano")]
-      : []),
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  purge: ["./public/**/*.html"],
+  purge: ['./public/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
-};
+}
